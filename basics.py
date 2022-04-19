a62aecd4041a7bcee6519de65bfef6c216447e85
TONY <supawat125947@gmail.com>
2022-04-20 01:26:05 +0700

    Blur image

--- basics.py
+++ basics.py
@@ -1,10 +1,19 @@
 import cv2 as cv
 
-img = cv.imread('./Resources/Photos/cat.jpg')
+img = cv.imread('./Resources/Photos/park.jpg')
 cv.imshow('Cat', img)
 
 # Converting to grayscale
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Gray', gray)
+# gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow('Gray', gray)
+
+# Blur: remove some of the noise that exists in an image
+# use gaussian blur technique
+# to increase blur -> increase the kernel size
+blur_k3 = cv.GaussianBlur(img, (3, 3), cv.BORDER_DEFAULT)
+cv.imshow('Blur kernel size 3', blur_k3)
+
+blur_k7 = cv.GaussianBlur(img, (7, 7), cv.BORDER_DEFAULT)
+cv.imshow('Blur kernel size 7', blur_k7)
 
 cv.waitKey(0)
