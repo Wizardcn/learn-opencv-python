c45b9f8b207d999c8d4db4054d2a512dd3f17e71
TONY <supawat125947@gmail.com>
2022-04-20 01:04:50 +0700

    Write text

--- draw.py
+++ draw.py
@@ -19,5 +19,10 @@ cv.circle(blank, (blank.shape[1]//2,
 # 4. Draw a line
 cv.line(blank, (10, 100), (blank.shape[1]//2,
         blank.shape[0]//2), (255, 255, 255), thickness=3)
-cv.imshow('Line', blank)
+
+# 5. Write text
+cv.putText(blank, 'Hello', (350, 225),
+           cv.FONT_HERSHEY_TRIPLEX, 1.0, (0, 255, 0), thickness=2)
+cv.imshow('Text', blank)
+
 cv.waitKey(0)
