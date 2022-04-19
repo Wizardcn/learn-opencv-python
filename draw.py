73fce19eb6c118897a3f6fb75b8abb0ef75a6401
TONY <supawat125947@gmail.com>
2022-04-20 00:59:49 +0700

    Draw a line

--- draw.py
+++ draw.py
@@ -11,11 +11,13 @@ blank = np.zeros((500, 500, 3), dtype='uint8')
 # 2. Draw a Rectangle
 cv.rectangle(blank, (0, 0),
              (blank.shape[1]//2, blank.shape[0]//2), (0, 255, 0), thickness=cv.FILLED)
-cv.imshow('Rectangle', blank)
 
 # 3. Draw a Circle
 cv.circle(blank, (blank.shape[1]//2,
           blank.shape[0]//2), 40, (0, 0, 255), thickness=3)
-cv.imshow('Circle', blank)
 
+# 4. Draw a line
+cv.line(blank, (10, 100), (blank.shape[1]//2,
+        blank.shape[0]//2), (255, 255, 255), thickness=3)
+cv.imshow('Line', blank)
 cv.waitKey(0)
