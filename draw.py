51e298d9312968ffc8809471cc82499dce9b775f
TONY <supawat125947@gmail.com>
2022-04-20 00:56:15 +0700

    Draw a Circle

--- draw.py
+++ draw.py
@@ -13,4 +13,9 @@ cv.rectangle(blank, (0, 0),
              (blank.shape[1]//2, blank.shape[0]//2), (0, 255, 0), thickness=cv.FILLED)
 cv.imshow('Rectangle', blank)
 
+# 3. Draw a Circle
+cv.circle(blank, (blank.shape[1]//2,
+          blank.shape[0]//2), 40, (0, 0, 255), thickness=3)
+cv.imshow('Circle', blank)
+
 cv.waitKey(0)
