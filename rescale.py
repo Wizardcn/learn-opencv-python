913d7311ee8b39fb592dce6d64500f4344137159
TONY <supawat125947@gmail.com>
2022-04-19 16:25:12 +0700

    change resolution

--- rescale.py
+++ rescale.py
@@ -4,6 +4,12 @@ from cv2 import INTER_AREA
 img = cv.imread('./Resources/Photos/cat_large.jpg')
 
 
+def changeResolution(capture, width, height):
+    # live video
+    capture.set(3, width)
+    capture.set(4, height)
+
+
 def rescaleFrame(frame, scale=0.75):
     width = int(frame.shape[1] * scale)
     height = int(frame.shape[0] * scale)
