d69e51fb268f2b1e538f03170e48fb573c7a6502
TONY <supawat125947@gmail.com>
2022-04-20 16:51:18 +0700

    BGR to HSV

--- spaces.py
+++ spaces.py
@@ -15,8 +15,14 @@ cv.imshow('Park', img)
 # Grayscale images show the distribution of pixel intensities at particular locations of the images.
 # cv.COLOR_BGR2GRAY = 6
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-print(cv.COLOR_BGR2GRAY)
-cv.imshow('Gray', gray)
+# print(cv.COLOR_BGR2GRAY)
+# cv.imshow('Gray', gray)
+
+
+# BGR to HSV
+# HSV is called Hue Saturation Value and is kind of based on how humans think and conceive of color.
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+cv.imshow('HSV', hsv)
 
 
 cv.waitKey(0)
