24a7adb959aa95ef14a718b2e2370467ce404b5e
TONY <supawat125947@gmail.com>
2022-04-20 14:38:59 +0700

    Contour detection using thresholding

--- contours.py
+++ contours.py
@@ -13,16 +13,19 @@ Coutours are basically the boundaries of objects, the line or curve that joins t
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
-blur = cv.GaussianBlur(gray, (5, 5), cv.BORDER_DEFAULT)
-cv.imshow('Blur', blur)
+# blur = cv.GaussianBlur(gray, (5, 5), cv.BORDER_DEFAULT)
+# cv.imshow('Blur', blur)
 
-canny = cv.Canny(blur, 125, 175)
-cv.imshow('Canny Edges', canny)
+# canny = cv.Canny(blur, 125, 175)
+# cv.imshow('Canny Edges', canny)
+
+ret, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY)
+cv.imshow('Thresh', thresh)
 
 
 # The way that is used to find the contours of an image is by using the find contours method.
 contours, hierarchies = cv.findContours(
-    canny, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+    thresh, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
 print(f'{len(contours)} contour(s) found.')
 
 cv.waitKey(0)
