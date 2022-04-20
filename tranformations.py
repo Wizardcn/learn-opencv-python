ec014efc6a4ed2e4ac9f024efae2de08f2878dd3
TONY <supawat125947@gmail.com>
2022-04-20 13:57:17 +0700

    Rotation

--- tranformations.py
+++ tranformations.py
@@ -1,27 +1,50 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('./Resources/Photos/park.jpg')
-
-# cv.imshow('Park', img)
-
-# Translation: is basically shifting an image along the x and y axis up, down, left, right
 # Use Affine Transformaions technique
 # https://neutrium.net/mathematics/basics-of-affine-transformation/
 
 
+def main():
+    img = cv.imread('./Resources/Photos/park.jpg')
+    # cv.imshow('Park', img)
+
+    # translated = translate(img, 100, 100)
+    # cv.imshow('Translated', translated)
+
+    rotated = rotate(img, 45)
+    cv.imshow('Rotated', rotated)
+
+    cv.waitKey(0)
+
+
 def translate(img, x, y):
+    '''
+        Translation: is basically shifting an image along the x and y axis up, down, left, right
+        - -x --> left
+        - -y --> up
+        - x --> right
+        - y --> down
+    '''
     transMat = np.float32([[1, 0, x], [0, 1, y]])
     dimensions = (img.shape[1], img.shape[0])
     return cv.warpAffine(img, transMat, dimensions)
 
 
-# -x --> left
-# -y --> up
-# x --> right
-# y --> down
+def rotate(img, angle, rotPoint=None):
+    """
+        Specify any rotation point that you'd like to rotate the image around 
+    """
+    (height, width) = img.shape[:2]
 
-translated = translate(img, 100, 100)
-cv.imshow('Translated', translated)
+    if rotPoint is None:
+        rotPoint = (width//2, height//2)
 
-cv.waitKey(0)
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width, height)
+
+    return cv.warpAffine(img, rotMat, dimensions)
+
+
+if __name__ == "__main__":
+    main()
