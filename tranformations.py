3084db7ab85f746299d9a6c097c686fd45c31215
TONY <supawat125947@gmail.com>
2022-04-20 14:00:38 +0700

    Resizing

--- tranformations.py
+++ tranformations.py
@@ -1,3 +1,4 @@
+from tkinter import CURRENT
 import cv2 as cv
 import numpy as np
 
@@ -12,8 +13,12 @@ def main():
     # translated = translate(img, 100, 100)
     # cv.imshow('Translated', translated)
 
-    rotated = rotate(img, 45)
-    cv.imshow('Rotated', rotated)
+    # rotated = rotate(img, 45)
+    # cv.imshow('Rotated', rotated)
+
+    # Resizing
+    resized = cv.resize(img, (500, 500), interpolation=cv.INTER_CUBIC)
+    cv.imshow('Resized', resized)
 
     cv.waitKey(0)
 
