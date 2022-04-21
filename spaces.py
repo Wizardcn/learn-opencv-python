37a5432d530dfee899ceec4cacbdc72c973be8e5
TONY <supawat125947@gmail.com>
2022-04-21 10:52:07 +0700

    HSV to BGR and HSV to grayscale

--- spaces.py
+++ spaces.py
@@ -30,19 +30,25 @@ cv.imshow('HSV', hsv)
 
 # BGR to L*a*b
 lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
-cv.imshow('LAB', lab)
+# cv.imshow('LAB', lab)
 
 # BGR isn't the current system that we use to represent colors outside of opencv.
 # Outside of opencv, we use the RGB format, which id kind of like the inverse of BGR.
 
 # BGR to RGB
 rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-cv.imshow('RGB', rgb)
+# cv.imshow('RGB', rgb)
 
-plt.imshow(rgb)
-plt.show()
+# plt.imshow(rgb)
+# plt.show()
 # matplotlib: plt.imshow() will not inverse input image.
 # opencv: cv.imshow() will assume input image is BGR space and inverse to RGB to display on the screen.
+
+# cannot convert grayscale image to HSV directly
+# the way you can do that convert the grayscale image to BGR and then convert the BGR image to HSV
+# HSV to BGR
+hsv_bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+cv.imshow('HSV --> BGR', hsv_bgr)
 
 
 cv.waitKey(0)
