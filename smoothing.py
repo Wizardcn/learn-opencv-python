321ad8057898e3500cd2e50dc9541f5640de5d96
TONY <supawat125947@gmail.com>
2022-04-21 19:44:59 +0700

    Gaussian Blur

--- smoothing.py
+++ smoothing.py
@@ -1,3 +1,4 @@
+from random import gauss
 import cv2 as cv
 
 img = cv.imread('./Resources/Photos/cats.jpg')
@@ -9,5 +10,9 @@ cv.imshow('Cats', img)
 # Averaging
 average = cv.blur(img, (7, 7))
 cv.imshow('Average Blur', average)
+
+# Gaussian Blur
+gauss = cv.GaussianBlur(img, (7, 7), 0)
+cv.imshow('Gaussian Blur', gauss)
 
 cv.waitKey(0)
