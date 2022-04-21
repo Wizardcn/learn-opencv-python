a915b50cba7ae53a8354ae585c8cf2e13df69799
TONY <supawat125947@gmail.com>
2022-04-21 15:35:59 +0700

    average blur

---
+++ smoothing.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+
+img = cv.imread('./Resources/Photos/cats.jpg')
+cv.imshow('Cats', img)
+
+# In generally, We smooth an image when it tends to have a lot of noise, and noise that's caused from camera sensors are basically problems in lighting when the image was taken.
+# We can smooth out the image or reduce some of the noise by applying some blurring method.
+
+# Averaging
+average = cv.blur(img, (7, 7))
+cv.imshow('Average Blur', average)
+
+cv.waitKey(0)
