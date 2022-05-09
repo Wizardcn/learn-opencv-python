6cf3fd09cad228c25f9bbf901ec92b02262cb1f3
TONY <supawat125947@gmail.com>
2022-05-09 15:36:54 +0700

    Sobel method

--- EdgeDetection.py
+++ EdgeDetection.py
@@ -14,4 +14,13 @@ lap = cv.Laplacian(gray, cv.CV_64F)
 lap = np.uint8(np.absolute(lap))
 cv.imshow('Laplacian', lap)
 
+# Sobel
+sobelx = cv.Sobel(gray, cv.CV_64F, 1, 0)
+sobely = cv.Sobel(gray, cv.CV_64F, 0, 1)
+cv.imshow('Sobel X', sobelx)
+cv.imshow('Sobel Y', sobely)
+
+combined_sobel = cv.bitwise_or(sobelx, sobely)
+cv.imshow('Cobined Sobel', combined_sobel)
+
 cv.waitKey(0)
