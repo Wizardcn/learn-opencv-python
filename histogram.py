afca2eddebfc35df57ab0147d72bb849bbe09458
TONY <supawat125947@gmail.com>
2022-05-09 12:05:59 +0700

    compute color histogram

--- histogram.py
+++ histogram.py
@@ -6,25 +6,36 @@ img = cv.imread('./Resources/Photos/cats.jpg')
 cv.imshow('Cats', img)
 
 
-print(img.shape[:2])
 blank = np.zeros(img.shape[:2], dtype='uint8')
-circle = cv.circle(
+mask = cv.circle(
     blank.copy(), (img.shape[1]//2, img.shape[0]//2), 100, 255, -1)
 
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Gray', gray)
+# gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow('Gray', gray)
 
-masked = cv.bitwise_and(gray, gray, mask=circle)
+masked = cv.bitwise_and(img, img, mask=mask)
 cv.imshow('Masked Image', masked)
 
 # grayscale histogram
-gray_hist = cv.calcHist([gray], [0], masked, [256], [0, 256])
+# gray_hist = cv.calcHist([gray], [0], masked, [256], [0, 256])
+# plt.figure()
+# plt.title('Grayscale Histogram')
+# plt.xlabel('Bins')
+# plt.ylabel('# of pixels')
+# plt.plot(gray_hist)
+# plt.xlim([0, 256])
+# plt.show()
+
+# Color histogram
+colors = ('b', 'g', 'r')
 plt.figure()
-plt.title('Grayscale Histogram')
+plt.title('Color Histogram')
 plt.xlabel('Bins')
 plt.ylabel('# of pixels')
-plt.plot(gray_hist)
-plt.xlim([0, 256])
+for i, col in enumerate(colors):
+    hist = cv.calcHist([img], [i], mask, [256], [0, 256])
+    plt.plot(hist, color=col)
+    plt.xlim([0, 256])
 plt.show()
 
 cv.waitKey(0)
