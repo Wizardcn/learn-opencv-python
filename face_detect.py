38bc372817f7e1c542a984b6fd24fe1466c66c6c
TONY <supawat125947@gmail.com>
2022-05-10 08:11:44 +0700

    face detecton with group people

--- face_detect.py
+++ face_detect.py
@@ -3,12 +3,12 @@ import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 
-img = cv.imread('./Resources/Photos/lady.jpg')
-cv.imshow('Person', img)
+img = cv.imread('./Resources/Photos/group 1.jpg')
+cv.imshow('Group of people', img)
 
 # 1 convert BGR image to grayscale image
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Person in Grayscale', gray)
+cv.imshow('Persons in Grayscale', gray)
 
 # face detection does not involve skin tone or the colors that are present in the image.
 # These hard cascades essentially look at an object in an image and using the edges tries to determine whether it's a face or not.
@@ -16,14 +16,31 @@ cv.imshow('Person in Grayscale', gray)
 # read haar_face.xml
 haar_cascade = cv.CascadeClassifier('haar_face.xml')
 
-faces_rect = haar_cascade.detectMultiScale(
-    gray, scaleFactor=1.1, minNeighbors=3)
 
-print(f'Number of faces found = {len(faces_rect)}')
-for (x, y, w, h) in faces_rect:
-    cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
+img1 = img.copy()
+minNeighbors = 3
+faces_rect1 = haar_cascade.detectMultiScale(
+    gray, scaleFactor=1.1, minNeighbors=minNeighbors)
+
+print(f'Number of faces found = {len(faces_rect1)}')
+for (x, y, w, h) in faces_rect1:
+    cv.rectangle(img1, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
+
+cv.imshow(f'Detected Faces with minimum neighbors = {minNeighbors}', img1)
+
+# Haar cascades are really sensitive to noise in an image
+# The way we can minimize the sensitivity to noise is essentially modifying the scale factor in minimum neighbors
+
+img2 = img.copy()
+minNeighbors = 6
+faces_rect2 = haar_cascade.detectMultiScale(
+    gray, scaleFactor=1.1, minNeighbors=minNeighbors)
+
+print(f'Number of faces found = {len(faces_rect2)}')
+for (x, y, w, h) in faces_rect2:
+    cv.rectangle(img2, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
 
 
-cv.imshow('Detected Faces', img)
+cv.imshow(f'Detected Faces with minimum neighbors = {minNeighbors}', img2)
 
 cv.waitKey(0)
